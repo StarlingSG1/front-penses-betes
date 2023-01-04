0815a330959dc3aeff66bae25d58a1631d028729
Jérémie <barriere.jeremie@gmail.com>
2023-01-04 19:55:00 +0100

    [fix] register

--- src/pages/register.page.js
+++ src/pages/register.page.js
@@ -51,46 +51,7 @@ export default function Register() {
         setHeadActive(3)
     }, [])
 
-   
-    //     return (
-    //         <>
-    //             <Header />
-    //             <main className="max-w-[800px] w-full mx-auto px-[2.5%]">
-    //                 <h2 className="text-center text-3xl font-montserrat font-medium my-10">Inscription</h2>
-
-    //                 <div className="w-full m-auto">
-    //                     <form onSubmit={(e) => { registerTheUser(e) }}>
-    //                         <div className=" gap-4 flex flex-col items-center relative bg-blue z-10">
-    //                             <div className=" flex sm:flex-row flex-col items-center sm:justify-between sm:gap-0 gap-4 w-full">
-    //                                 <Input
-    //                                     type="text"
-    //                                     className={"sm:w-[47.5%]"}
-    //                                     placeholder={"Prénom"}
-    //                                     onChange={(e) => { setUserCredentials({ ...userCredentials, firstName: e.target.value }) }}
-    //                                 />
-    //                                 <Input type="text" className={"sm:w-[47.5%]"} placeholder={"Nom"} onChange={(e) => { setUserCredentials({ ...userCredentials, lastName: e.target.value }) }} />
-    //                             </div>
-    //                             <Input type="email" placeholder={"Adresse mail"} onChange={(e) => { setUserCredentials({ ...userCredentials, email: e.target.value }) }} />
-    //                             <Input type="password" placeholder={"Mot de passe"} onChange={(e) => { setUserCredentials({ ...userCredentials, password: e.target.value }) }} />
-    //                             <Input type="password" placeholder={"Confirmer mot de passe"} onChange={(e) => { setUserCredentials({ ...userCredentials, confirmPassword: e.target.value }) }} />
-    //                             <div className="flex items-center w-full gap-2">
-    //                                 <input type="checkbox" className="w-[18px] aspect-square h-[18px] border border-gray-400 !text-red accent-red rounded-sm" onChange={(e) => { setUserCredentials({ ...userCredentials, conditions: e.target.checked }) }} />
-    //                                 <Paragraph className="text-sm  text-gray-400  text-base">J'accepte les <Link to="/politique-de-confidentialite" className="!text-base">conditions générales d'utilisation</Link>.</Paragraph>
-    //                             </div>
-    //                         </div>
-
-    //                         <div className="flex sm:flex-row flex-col-reverse justify-between items-center relative mt-[25px] z-10 pl-8 pr-[15px]">
-    //                             <span className="absolute h-[50px] w-[50px] border-[5px] rounded-full sm:right-[4px] sm:-bottom-[10px] hidden sm:block border-white -z-10"></span>
-    //                             <Link to="/login" className="font-montserrat px-5 py-1 border-2 border-primary rounded-lg text-primary font-medium">J'ai déjà un compte</Link>
-    //                             <span className="sm:w-[1px] sm:h-[35px] h-[1px] w-[270px] sm:py-0 my-5 bg-white"></span>{" "}
-    //                             <Button type="submit">{!loading ? "Créer mon compte" : "Création..."}</Button>
-    //                         </div>
-    //                     </form>
-    //                 </div>
-    //             </main>
-    //         </>
-    //     );
-    // }
+    
     return (
         <Template>
             <Title className="mb-10 sm:mb-[60px]">Inscription</Title>
